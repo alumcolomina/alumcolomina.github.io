051ee87ca94f7486f5ced1e64b17b0b6505136e2
alumcolomina <lhintermullerescobedo@alumnat.iessacolomina.es>
2024-05-17 13:26:49 +0200

    initial comit

---
+++ script.js
@@ -0,0 +1,71 @@
+function mostrarmenu() {
+    var elemento = document.getElementById("mostrar");
+        elemento.classList.toggle("menu");
+}
+
+function a() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '1.png';
+    //cambiar scr="" de la imagen
+}
+function b() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '2.png';
+}
+function c() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '3.png';
+    //cambiar scr="" de la imagen
+}
+function d() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '4.png';
+}function e() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '5.png';
+    //cambiar scr="" de la imagen
+}
+function f() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '6.png';
+}function g() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '7.png';
+    //cambiar scr="" de la imagen
+}
+function h() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '8.png';
+}function i() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '9.png';
+    //cambiar scr="" de la imagen
+}
+function j() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '10.png';
+}function k() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '11.png';
+    //cambiar scr="" de la imagen
+}
+function l() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '12.png';
+}function m() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '13.png';
+    //cambiar scr="" de la imagen
+}
+function n() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '14.png';
+}function o() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '15.png';
+    //cambiar scr="" de la imagen
+}
+function p() {
+    const imagen = document.getElementById('foto');
+    imagen.src = '16.png';
+}
